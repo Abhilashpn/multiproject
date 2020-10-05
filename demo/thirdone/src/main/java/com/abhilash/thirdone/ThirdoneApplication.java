deac47878c22a6d68fd375db2ed60ef0803fcbd4
Abhilash <abhiputhumana@gmail.com>
2020-10-05 18:18:46 +0530

    Initial commit

---
+++ demo/thirdone/src/main/java/com/abhilash/thirdone/ThirdoneApplication.java
@@ -0,0 +1,13 @@
+package com.abhilash.thirdone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdoneApplication.class, args);
+	}
+
+}
---
+++ demo/thirdone/src/test/java/com/abhilash/thirdone/ThirdoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhilash.thirdone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
